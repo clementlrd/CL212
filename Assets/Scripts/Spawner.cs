ed9e90d3b3e5b179215f94e31b37cb26374f7ce4
Antoine DOMINGUES <antoine.domingues@ensta-paris.fr>
2022-12-23 00:57:00 +0100

    rewrite script with bubble class

--- Assets/Scripts/Spawner.cs
+++ Assets/Scripts/Spawner.cs
@@ -2,11 +2,44 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+public class Bubble : Object
+{
+    public GameObject sphere;
+    // Start is called before the first frame update
+    public Bubble(Material materialRef, Vector3 floatSpeed)
+    {
+        sphere = GameObject.CreatePrimitive(PrimitiveType.Sphere);
+        //Add physics to the bubble
+        Rigidbody currentRb;
+        currentRb = sphere.AddComponent<Rigidbody>();
+        currentRb.detectCollisions = true;
+        currentRb.useGravity = false; //Disable gravity to float
+        currentRb.velocity = floatSpeed; //Go to floatSpeed
+        //Set material
+        sphere.GetComponent<Renderer>().material = materialRef;
+        //Generate a random size
+        System.Random rd = new System.Random();
+        double randomValue = rd.NextDouble() * (1 - 0.1) + 0.1; //To have a float between 0.1 and 1
+        Vector3 newScale = new Vector3((float)randomValue, (float)randomValue, (float)randomValue);
+        sphere.transform.localScale = newScale;
+    }
+    void OnCollisionEnter(Collision col)
+    {
+        if (col.gameObject.GetComponent<Renderer>().material.name == "BubbleBW" || col.gameObject.GetComponent<Renderer>().material.name == "BubbleC")
+        {
+
+        }
+        else
+        {
+            Destroy(col.gameObject);
+        };
+    }
+}
 
 public class Spawner : MonoBehaviour
 {
     public Material materialRef;
-
+    public Vector3 floatSpeed;
 
     // Start is called before the first frame update
     void Start()
@@ -19,33 +52,9 @@ public class Spawner : MonoBehaviour
     {
         //Wait 2 seconds before spawning
         System.Threading.Thread.Sleep(2000);
-        GameObject sphere = GameObject.CreatePrimitive(PrimitiveType.Sphere);
-        //Add physics to the bubble
-        Rigidbody currentRb;
-        currentRb = sphere.AddComponent<Rigidbody>();
-        currentRb.detectCollisions = true;
-        currentRb.useGravity = false; //Disable gravity to float
-        currentRb.velocity = new Vector3(0.0f, 1.0f, 0.0f); //Go up with speed of 1
-        void OnCollisionEnter(Collision col)
-        {
-            if (col.gameObject.GetComponent<Renderer>().material.name == "BubbleBW" || col.gameObject.GetComponent<Renderer>().material.name == "BubbleC")
-            {
-
-            }
-            else
-            {
-                Destroy(col.gameObject);
-            };
-        }
-        //Set material
-        sphere.GetComponent<Renderer>().material = materialRef;
-        //Generate a random size
-        System.Random rd = new System.Random();
-        double randomValue = rd.NextDouble() * (1-0.1)+0.1; //To have a float between 0.1 and 1
-        Vector3 newScale = new Vector3((float)randomValue, (float)randomValue, (float)randomValue);
-        sphere.transform.localScale = newScale;
-
-        Instantiate(sphere, sphere.transform.position, Quaternion.identity);
+        Bubble new_bubble = new Bubble(materialRef,floatSpeed);
+        GameObject new_object = new_bubble.sphere;
+        Instantiate(new_object,new_object.transform.position,Quaternion.identity);
         
     }
 }
