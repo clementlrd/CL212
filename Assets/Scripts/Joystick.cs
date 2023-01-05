69044e70fcfe44c38a14f9064ef11c9bf371b494
clément LAROUDIE <clement.laroudie@ensta-paris.fr>
2023-01-05 22:35:36 +0100

    add moumni to the party

--- Assets/Scripts/Joystick.cs
+++ Assets/Scripts/Joystick.cs
@@ -6,14 +6,14 @@ using OVR;
 public class Joystick : MonoBehaviour
 {
     public float speed;
-    
+
     void Update()
     {
         var joystickAxis = OVRInput.Get(OVRInput.RawAxis2D.LThumbstick, OVRInput.Controller.LTouch);
         
         float fixedY = gameObject.transform.position.y;
         
-        gameObject.transform.position += (transform.right * joystickAxis.x + transform.forward * joystickAxis.y) * Time.deltaTime * speed;
+        gameObject.transform.position += (gameObject.transform.right * joystickAxis.x + gameObject.transform.forward * joystickAxis.y) * Time.deltaTime * speed;
         gameObject.transform.position = new Vector3(gameObject.transform.position.x, fixedY, gameObject.transform.position.z);
         
     }
