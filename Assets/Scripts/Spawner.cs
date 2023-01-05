0f15b8d24a0a1dd2fad39567811f279980fe9cf2
Antoine DOMINGUES <antoine.domingues@ensta-paris.fr>
2023-01-05 16:17:38 +0100

    correct script spawner

--- Assets/Scripts/Spawner.cs
+++ Assets/Scripts/Spawner.cs
@@ -6,7 +6,7 @@ public class Bubble : Object
 {
     public GameObject sphere;
     // Start is called before the first frame update
-    public Bubble(Material materialRef, Vector3 floatSpeed)
+    public Bubble(Material materialRef)
     {
         sphere = GameObject.CreatePrimitive(PrimitiveType.Sphere);
         //Add physics to the bubble
@@ -14,47 +14,53 @@ public class Bubble : Object
         currentRb = sphere.AddComponent<Rigidbody>();
         currentRb.detectCollisions = true;
         currentRb.useGravity = false; //Disable gravity to float
-        currentRb.velocity = floatSpeed; //Go to floatSpeed
         //Set material
         sphere.GetComponent<Renderer>().material = materialRef;
         //Generate a random size
         System.Random rd = new System.Random();
-        double randomValue = rd.NextDouble() * (1 - 0.1) + 0.1; //To have a float between 0.1 and 1
+        double randomValue = rd.NextDouble() * (1 - 0.1) + 0.1; //To have a double between 0.1 and 1
         Vector3 newScale = new Vector3((float)randomValue, (float)randomValue, (float)randomValue);
         sphere.transform.localScale = newScale;
+        double randomSpeed1 = rd.NextDouble() * 2 -1; //To have a double between -1 and 1
+        double randomSpeed2 = rd.NextDouble() * (1-0.1) + 0.1; // To have y>0 to not go down
+        double randomSpeed3 = rd.NextDouble() * 2 - 1;
+        Vector3 newSpeed = new Vector3((float)randomSpeed1, (float)randomSpeed2, (float)randomSpeed3);
+        currentRb.velocity = newSpeed ;
     }
     void OnCollisionEnter(Collision col)
     {
-        if (col.gameObject.GetComponent<Renderer>().material.name == "BubbleBW" || col.gameObject.GetComponent<Renderer>().material.name == "BubbleC")
-        {
-
+        switch (col.gameObject.GetComponent<Renderer>().material.name) {
+            case "BubbleBW":
+            case "BubbleC":
+                break;
+            default:
+                Destroy(col.gameObject);
+                break;
         }
-        else
-        {
-            Destroy(col.gameObject);
-        };
     }
 }
 
 public class Spawner : MonoBehaviour
 {
     public Material materialRef;
-    public Vector3 floatSpeed;
+    private int compteur;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        compteur = 0;
     }
 
     // Update is called once per frame
     void Update()
     {
-        //Wait 2 seconds before spawning
-        System.Threading.Thread.Sleep(2000);
-        Bubble new_bubble = new Bubble(materialRef,floatSpeed);
-        GameObject new_object = new_bubble.sphere;
-        Instantiate(new_object,new_object.transform.position,Quaternion.identity);
+        if (compteur == 100)
+        {
+            Bubble new_bubble = new Bubble(materialRef);
+            new_bubble.sphere.transform.parent = gameObject.transform;
+            compteur = 0;
+        }
+        else { compteur++; }
         
     }
 }
